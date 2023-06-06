4f68e4d867ba51732da7f41775acf76fafa39c10
EdgardOrd <edgard.ordonez26@gmail.com>
2023-06-06 07:31:11 -0600

    Adding pagination to tables and changing ways to fetch api

--- src/components/LaptopTable.js
+++ src/components/LaptopTable.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment';
 import '../style/Table.css';
 
-function LaptopTable({ searchText, apiUrl }) {
+function LaptopTable({ searchText }) {
   const [data, setData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
-    fetch("https://reqres.in/api/unknown")
-      .then((response) => response.json())
-      .then((responseData) => {
-        const apiData = responseData.data || []; // Acceder a la propiedad 'data' o establecer un arreglo vacío por defecto
-        setData(apiData);
-        setFilteredData(apiData); // Mostrar todos los datos sin filtrar inicialmente
-      });
-  }, [apiUrl]);
+    const fetchData = async () => {
+      try {
+        const response = await fetch("");
+        if (response.ok) {
+          const jsonData = await response.json();
+          const apiData = jsonData.data || [];
+          setData(apiData);
+          setFilteredData(apiData);
+        } else {
+          console.error('Error fetching data:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   useEffect(() => {
     const filtered = data.filter((row) => {
@@ -34,28 +47,54 @@ function LaptopTable({ searchText, apiUrl }) {
     setFilteredData(filtered);
   }, [data, searchText]);
 
-  const headers = Object.keys(filteredData[0] || {});
+  const headers = ["id", "series", "charger", "guest", "state", "record"];
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+  const changePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
-    <table className="">
-      <thead>
-        <tr>
-          {headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {filteredData.map((row) => (
-          <tr key={row.id}>
+    <div className="table-container">
+      <table className="responsive-table">
+        <thead>
+          <tr>
             {headers.map((header) => (
-              <td key={header}>{row[header]}</td>
+              <th key={header}>{header}</th>
             ))}
           </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((row) => (
+            <tr key={row.id}>
+              {headers.map((header) => (
+                <td key={header}>
+                  {header === 'record'
+                    ? moment(row[header]).format('MMMM Do, YYYY')
+                    : row[header]}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <div className="pagination">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button
+            key={index}
+            onClick={() => changePage(index + 1)}
+            className={currentPage === index + 1 ? 'active' : ''}
+          >
+            {index + 1}
+          </button>
         ))}
-      </tbody>
-    </table>
+      </div>
+    </div>
   );
 }
-
 export default LaptopTable;
